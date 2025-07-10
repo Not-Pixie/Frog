e23172fbf9a6c9fad0c667b399b681d6bac1cd4a
Miguel Azocar <miguelcarmoazocar@gmail.com>
2025-07-10 18:08:28 -0300

    Tentativa de configuração para o sistema de login.

---
+++ backend/app/services/userservice.py
@@ -0,0 +1,12 @@
+from sqlalchemy.orm import Session
+from backend.app.models.usermodel import Usuario
+
+def listar_usuarios(db: Session):
+    return db.query(Usuario).all()
+
+def criar_usuario(db: Session, usuario_data: dict):
+    novo_usuario = Usuario(**usuario_data)
+    db.add(novo_usuario)
+    db.commit()
+    db.refresh(novo_usuario)
+    return novo_usuario
--- backend/main.py
+++ backend/main.py
@@ -1,2 +1,18 @@
-from app.config import engine
+from backend.config import engine
+import os
+
 print("Conectado com sucesso ao banco!") if engine else print("Erro")
+
+from flask import Flask
+from flask_cors import CORS
+from backend.app.routes.userroutes import usuario_bp
+from backend.app.models.usermodel import Base
+
+app = Flask(__name__)
+CORS(app)
+app.register_blueprint(usuario_bp)
+Base.metadata.create_all(bind=engine)
+
+if __name__ == "__main__":
+    port = int(os.getenv("PORT", 3001))
+    app.run(host="0.0.0.0", port=port)
