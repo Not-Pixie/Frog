adda156e3945a71bfa5cc6c2bab706f9566a279b
Miguel Azocar <miguelcarmoazocar@gmail.com>
2025-08-08 11:43:28 -0300

    Adiciona a pasta 'utils' e atualiza os triggers.

--- backend/app/database/2 - others/50 functions.sql
+++ backend/app/database/2 - others/50 functions.sql
@@ -1,31 +1,33 @@
 CREATE OR REPLACE FUNCTION fn_set_timestamp() RETURNS TRIGGER AS $$
 BEGIN
-    NEW.updated_at = NOW();
+    NEW.atualizado_em = NOW();
     RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
-
 CREATE OR REPLACE FUNCTION fn_log_updates() RETURNS TRIGGER AS $$
+DECLARE
+    usuario_atual INTEGER := current_setting('app.usuario_id', true)::INTEGER;
 BEGIN
     IF TG_OP = 'UPDATE' THEN
-        INSERT INTO change_log(table_name, record_id, operation, changed_by, old_data, new_data)
+        INSERT INTO logs(tabela_nome, record_id, operacao, alterado_por, antigo_dado, novo_dado)
         VALUES (
             TG_TABLE_NAME,
             COALESCE(OLD.id, NEW.id),
             TG_OP,
-            NULL,
+            usuario_atual,
             to_jsonb(OLD),
             to_jsonb(NEW)
         );
         RETURN NEW;
+
     ELSIF TG_OP = 'DELETE' THEN
-        INSERT INTO change_log(table_name, record_id, operation, changed_by, old_data, new_data)
+        INSERT INTO logs(tabela_nome, record_id, operacao, alterado_por, antigo_dado, novo_dado)
         VALUES (
             TG_TABLE_NAME,
             OLD.id,
             TG_OP,
-            NULL,
+            usuario_atual,
             to_jsonb(OLD),
             NULL
         );
