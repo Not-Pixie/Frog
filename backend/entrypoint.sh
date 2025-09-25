68d1d8e9d34ac308fd1a85a51f3bccfb570d1697
Miguel Azocar <miguelcarmoazocar@gmail.com>
2025-09-25 11:43:21 -0300

    Converte CRLF para LF no sistema.

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+﻿#!/bin/sh
 set -e
 
 # espera o Postgres ficar pronto (usa pg_isready)
-# ajuste DB_HOST/DB_PORT/DB_USER se necessário
+# ajuste DB_HOST/DB_PORT/DB_USER se necessÃ¡rio
 DB_HOST=${DB_HOST:-db}
 DB_PORT=${DB_PORT:-5432}
 DB_USER=${DB_USER:-postgres}
@@ -17,8 +17,9 @@ echo "Postgres pronto."
 echo "Aplicando migrations (alembic upgrade head)..."
 alembic upgrade head
 
-# Por segurança, você pode também garantir create_all em dev:
+# Por seguranÃ§a, vocÃª pode tambÃ©m garantir create_all em dev:
 # python -c "from app.database.database import Base, engine; import app.models; Base.metadata.create_all(bind=engine)"
 
-# finalmente, executa o comando padrão do container (por ex. gunicorn/flask run)
+# finalmente, executa o comando padrÃ£o do container (por ex. gunicorn/flask run)
 exec "$@"
+
