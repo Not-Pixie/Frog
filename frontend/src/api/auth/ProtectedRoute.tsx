8b6eb503fb5af28f46249ec385502f7819a9c74a
Nicolas <nicolashenrique1225@gmail.com>
2025-10-19 12:58:30 -0300

    Adiciona LoadingPage cnetralizada

--- frontend/src/api/auth/ProtectedRoute.tsx
+++ frontend/src/api/auth/ProtectedRoute.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Navigate } from 'react-router';
 import { useAuth } from './AuthProvider';
+import LoadingPage from 'src/pages/LoadingPages';
 
 export default function ProtectedRoute({ children }:{ children: React.ReactNode }) {
   const { user, loading } = useAuth();
-  if (loading) return <div>Carregando...</div>;
+  if (loading) return <LoadingPage/>;
   if (!user) return <Navigate to="/entrar" replace />;
   return children;
 }
--- frontend/src/api/auth/PublicRoute.tsx
+++ frontend/src/api/auth/PublicRoute.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { useAuth } from './AuthProvider';
+import LoadingPage from 'src/pages/LoadingPages';
 
 export default function PublicRoute({ children, redirectIfAuthenticated }:{ children: React.ReactNode, redirectIfAuthenticated?: boolean }) {
   if (!redirectIfAuthenticated) redirectIfAuthenticated = false;
   const { user, loading } = useAuth();
-  if (loading) return <div>Carregando...</div>;
+  if (loading) return <LoadingPage/>;
   if (user && redirectIfAuthenticated) return children;
   return children;
 }
