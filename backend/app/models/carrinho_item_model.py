2332af172ac69dc9be76fd93305c34fef068b091
Nicolas <nicolashenrique1225@gmail.com>
2025-11-14 11:47:36 -0300

    Move decsonto percentual para carrinho_item

--- backend/app/models/carrinho_item_model.py
+++ backend/app/models/carrinho_item_model.py
@@ -10,13 +10,14 @@ class CarrinhoItem(Base):
     carrinho_id = Column(Integer, ForeignKey("carrinhos.carrinho_id", ondelete="CASCADE", onupdate="CASCADE"), nullable=False)
     produto_id = Column(Integer, ForeignKey("produtos.produto_id", ondelete="RESTRICT", onupdate="CASCADE"), nullable=False)
     quantidade = Column(Integer, nullable=False, default=1)
+    desconto_percentual = Column(Numeric(6, 4), nullable=True) # Desconto em percentual (ex: 10.5% = 10.5)
 
     criado_em = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
 
     comercio_id = Column(Integer, ForeignKey("comercios.comercio_id", ondelete="CASCADE"), nullable=False)
 
     carrinho = relationship("Carrinho", back_populates="itens", lazy="joined")
-    produto = relationship("Produto", lazy="joined")  # já existe na sua model
+    produto = relationship("Produto", lazy="joined") 
     comercio = relationship("Comercio", back_populates="carrinhoitens" )
 
     __table_args__ = (
--- backend/app/models/movimentacao_model.py
+++ backend/app/models/movimentacao_model.py
@@ -7,29 +7,25 @@ class Movimentacao(Base):
     __tablename__ = "movimentacoes"
 
     mov_id = Column(Integer, primary_key=True, autoincrement=True)
-
     tipo = Column(String(7), nullable=False)  # 'entrada' = 7 chars, 'saida' = 5 chars
-    
-    # Add the check constraint to the table
-    __table_args__ = (
-        CheckConstraint("tipo IN ('entrada','saida')", name='ck_movimentacoes_tipo'),
-    )
-
     link = Column(String(16), nullable=False, unique=True)
 
-    # ligação com carrinho (nullable caso queira registrar movimentação sem carrinho)
     carrinho_id = Column(Integer, ForeignKey("carrinhos.carrinho_id", ondelete="CASCADE", onupdate="CASCADE"), nullable=False)
 
-    # valor final armazenado (imutar por segurança/histórico)
     valor_total = Column(Numeric(12, 4), nullable=False)
 
-    # forma de pagamento e desconto em percentual (ex: 10.5% = 10.5)
+    # forma de pagamento e 
     forma_pagamento = Column(String(100), nullable=True)
-    desconto_percentual = Column(Numeric(6, 4), nullable=True)
+    
 
     comercio_id = Column(Integer, ForeignKey("comercios.comercio_id", ondelete="CASCADE"), nullable=False)
 
     criado_em = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
+    
+     # Add the check constraint to the table
+    __table_args__ = (
+        CheckConstraint("tipo IN ('entrada','saida')", name='ck_movimentacoes_tipo'),
+    )
 
     # relacionamentos (opcionais)
     carrinho = relationship("Carrinho", lazy="joined")
