7022fd4122d473cba273e8923235f5148a369fc0
Nicolas <nicolashenrique1225@gmail.com>
2025-11-14 10:54:43 -0300

    Cria services para criação vazia de carrinho e movimentações

--- backend/app/services/movimentacao_service.py
+++ backend/app/services/movimentacao_service.py
@@ -1,22 +1,26 @@
-from decimal import Decimal, ROUND_HALF_UP
 from sqlalchemy.orm import Session
 from app.models.carrinho_model import Carrinho, CarrinhoItem, Movimentacao
 
 
-def criar_carrinho_vazio(db: Session) -> Carrinho:
+def criar_carrinho_vazio(db: Session, comercio_id: int) -> Carrinho:
+    car = Carrinho(
+        comercio_id=comercio_id
+    )
+    return car
+
+
+def criar_movimentacao_vazia(db: Session, tipo: str, comercio_id: int) -> Movimentacao:
+    carrinho = criar_carrinho_vazio(db, comercio_id)
     
-    return carrinho
-
-
-def criar_movimentacao_vazia(db: Session, tipo: str,) -> Movimentacao:
-
     mov = Movimentacao(
         tipo=tipo,
-        carrinho_id=carrinho_id,
+        carrinho_id=carrinho.carrinho_id,
+        comercio_id = comercio_id,
         valor_total=0,
-        forma_pagamento=forma_pagamento,
-        desconto_percentual=desconto_percentual
+        forma_pagamento="",
+        desconto_percentual=0
     )
+    
     db.add(mov)
     db.commit()
     db.refresh(mov)
