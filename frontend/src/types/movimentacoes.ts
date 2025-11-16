6b4d408e01e970198f3adb4b77d2668c8c41a873
Nicolas <nicolashenrique1225@gmail.com>
2025-11-15 21:25:21 -0300

    Cria tipo para movimentações

---
+++ frontend/src/types/movimentacoes.ts
@@ -0,0 +1,9 @@
+export type Movimentacoes = {
+    mov_id: number,
+    tipo: string,
+    link: string,
+    carrinho_id: number,
+    valor_total: number,
+    comercio_id: number,
+    criado_em: string
+}
